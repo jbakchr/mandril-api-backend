5676529f2849c0618a53e6f739dab99e4f086d7e
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-01 14:26:42 +0200

    Added getting all characters

--- main.py
+++ main.py
@@ -31,6 +31,22 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/characters")
 def read_item():
-    with open("./data/characters.json") as f:
-        characters = json.loads(f.read())
-    return characters
+    con = sqlite3.connect("mandril.db")
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM characters")
+
+    characters = cur.fetchall()
+
+    con.close()
+
+    res_json = []
+    for character in characters:
+        item = {
+            "character_id": character[0],
+            "character_name": character[1],
+            "character_desc": character[2],
+        }
+        res_json.append(item)
+
+    return res_json
