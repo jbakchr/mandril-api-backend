0c3d4f1b1b23884cb141e9a10c1f416178e66122
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-02 11:36:23 +0200

    Added episodes table to db

--- db/create_db.sql
+++ db/create_db.sql
@@ -20,3 +20,11 @@ CREATE TABLE IF NOT EXISTS character_actor
   FOREIGN KEY (character_id) REFERENCES characters(character_id),
   FOREIGN KEY (actor_id) REFERENCES actors(actor_id)
 );
+
+CREATE TABLE IF NOT EXISTS episodes
+(
+  episode_id INTEGER NOT NULL,
+  season INTEGER NOT NULL,
+  episode INTEGER NOT NULL,
+  PRIMARY KEY (episode_id)
+);
